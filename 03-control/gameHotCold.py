720a35971606808615c286b1c1e0abe09492bb5e
agrebowiec <artur.grebowiec@gmail.com>
2022-03-01 22:07:37 +0100

    Exercises 03-control; game.

---
+++ 03-control/gameHotCold.py
@@ -0,0 +1,13 @@
+import random
+computer = random.randrange(0, 100)
+user = 0
+steps = 6
+print("\nGame - guess number 0-100 max in 6 steps!")
+for i in range(steps):
+    print(f'Attempt {i + 1}. ', end="")
+    user = int(input("Type your number:.."))
+    if user == computer:
+        print("You win!")
+    else:
+        print("Hot") if user < computer else print("Cold")
+print("You lose.")
