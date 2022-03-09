41853b0b9ba21436291d9ba6535bce7c07646fcd
agrebowiec <artur.grebowiec@gmail.com>
2022-03-09 17:08:54 +0100

    Exercises 04-collections; fixed.

--- 04-collections/end04multiT.py
+++ 04-collections/end04multiT.py
@@ -1,11 +1,11 @@
 n = 10
-print("Multiplication Tables")
-multiT = [[0]*n] * n
-print(multiT)
+print("\nMultiplication Tables")
+# multiT = [[0]*n] * n
+multiT = [[0 for x in range(n)] for y in range(n)]
 
 for x in range(1, n+1):
     for y in range(1, n+1):
-        multiT[x-1][y-1] = x * y
+        multiT[x-1][y-1] = x*y
 
 for row in multiT:
     for col in row:
