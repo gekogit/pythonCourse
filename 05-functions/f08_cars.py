c025fd50b6705e1abdef14ddec85ddf863f3eb70
agrebowiec <artur.grebowiec@gmail.com>
2022-03-11 17:38:28 +0100

    ok

--- 05-functions/f08_cars.py
+++ 05-functions/f08_cars.py
@@ -11,3 +11,20 @@ Sprawdzi, czy samochód ma minimum 25 lat. Jeśli tak, wypisze komunikat:
  “Twój samochód (tutaj_marka) jest jeszcze zbyt młody.”
     Gdy program będzie poprawnie działał, pozmieniaj wartości słownika (ale nie klucze!), aby zobaczyć, czy progam również zmienia swoje zachowanie.
 """
+
+
+def print_dict(dictionary):
+    print(dictionary)
+
+
+today_year = 2022
+car_dict = {
+    'brand': 'Audi',
+    'model': 'A4',
+    'year': 2020
+}
+print_dict(car_dict)
+if (today_year - car_dict['year']) > 25:
+    print('Car has more then 25 years.')
+else:
+    print('Your car', car_dict['brand'], 'is quite young.')
