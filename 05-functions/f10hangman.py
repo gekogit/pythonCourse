4a28250082de415e7f04080952e52ec587bc7dbb
agrebowiec <artur.grebowiec@gmail.com>
2022-03-11 16:25:26 +0100

    ok

--- 05-functions/f10hangman.py
+++ 05-functions/f10hangman.py
@@ -15,25 +15,29 @@ def word_random():
     words_base = ['Sarah', 'Anna', 'Julia', 'Laura', 'Lena', 'Hannah', 'Lisa', 'Katharina', 'Leonie', 'Vanessa']
     return words_base[random.randrange(0, len(words_base))]
 
-def print_blanks(word):
-    return '-'*len(word)
 
+repeat_number = 10
 
-repeat_number = 5
+computer_words = word_random()
+print('for those who likes to cheat:', computer_words)
+user_shadow_word = '-' * len(computer_words)
+print('\n\n\nGuess the word (ps.cap letter sensitive):')
+print(user_shadow_word)
 for n in range(repeat_number):
-    computer_words = word_random()
-    user_shadow_words = '-' * len(computer_words)
-    print(computer_words)
-    print(user_shadow_words)
-    print('Words to guess: ', print_blanks(computer_words))
-    user_letter = input('Type your letter or whole word:')
+    user_letter = input('Your letter:')
     if user_letter == computer_words:
         print('Well done')
         break
     for letter_index in range(len(computer_words)):
+        letter_in_word = False
         if computer_words[letter_index] == user_letter:
-            user_shadow_words[letter_index] = user_letter
-    print(user_shadow_words)
-
+            user_shadow_word = user_shadow_word[:letter_index] + user_letter + user_shadow_word[letter_index + 1:]
+            letter_in_word = True
+    if letter_in_word:
+        print(f'Bravo {user_letter} is in the word.')
+    print(user_shadow_word)
+    if user_shadow_word == computer_words:
+        print('Well done')
+        break
     if n == repeat_number:
         print('Ups... Try again next time.')
