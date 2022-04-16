113db40951dd9b3c89aa01c00634a2c8d5161686
agrebowiec <artur.grebowiec@gmail.com>
2022-04-16 13:47:18 +0200

    Aku, time to do

--- BMS/aku.py
+++ BMS/aku.py
@@ -22,23 +22,39 @@ def check_serial_open(ser):
 def to_dec(data):
     data_sp = data.decode("utf-8")
     data_sp = data_sp.split(',')
-    data_sp[1] = int(data_sp[1])
-    data_sp[2] = int(data_sp[2])
-    return data_sp
+    for el in range(1, len(data_sp)-2):
+        safe_line(str(int(data_sp[el], 16))+ ',')
+    safe_line('\n')
+
+
+def file_init():
+    with open("aku_log.txt", 'a', encoding='utf-8') as log_data:
+        log_data.write('v\ta\ttemp\tv1\tv2\tv3\tv4\tb\to\n')
+
+
+def safe_line(data):
+    with open("aku_log.txt", 'a', encoding='utf-8') as log_data:
+        log_data.write(data)
 
 
 def main():
+    print('BMS log BTO_14,6V_200Ah')
+    file_init()
     ser_port = serial_init()
     check_serial_open(ser_port)
+    counter = 0
     while True:
-        value = 0x1A4F
         ser_port.write(b'\x1A\x4F')
         time.sleep(1)
         bytes_to_read = ser_port.inWaiting()
-        data = ser_port.read(bytes_to_read)
-        time.sleep(1)
+        if bytes_to_read == 73:
+            data = ser_port.read(bytes_to_read)
+            to_dec(data)
+            counter += 1
+            print('Sample ', counter)
+            time.sleep(1)
         print(data)
-        #print(to_dec(data))
+
 
 
 if __name__ == '__main__':
